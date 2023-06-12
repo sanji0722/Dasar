e2e6a100afb9aa5834366061ead0c8dc0082af61
sanji0722 <saepullohepul565@gmail.com>
2023-06-13 01:50:53 +0700

    data Numbers

--- variable.dart
+++ variable.dart
@@ -1,6 +1,11 @@
 void main(){
 
   var nilai = 100; // inisialisasi langsung
+  int data_integer = 104;
+  double data_desimal = 0.90;
+
+  print(data_integer);
+  print(data_desimal);
 
   print(nilai);
 }
