2c8f9b4144a61045d5213611c732a9ccd0313bf5
sanji0722 <saepullohepul565@gmail.com>
2023-06-14 05:20:36 +0700

    Terus Belajar yah

---
+++ tipe_data.dart
@@ -0,0 +1,11 @@
+///Belajar tipe data yang ada di dart
+
+void main(){
+//tipe data num
+num nilai = 1;
+//num bisa bernilai integer dan double
+num nilai_2 = 0.4;
+
+print(nilai);
+print(nilai_2);
+}
